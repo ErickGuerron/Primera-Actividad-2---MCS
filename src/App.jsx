e6f9cf1318bd689316c073327e1f8736e8fe1085
ErickGuerron <168254828+ErickGuerron@users.noreply.github.com>
2025-08-27 09:56:16 -0500

    About me parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 import { AboutSection } from "./pages/mabe/AboutMe";
+import { AboutErick } from "./pages/erick/AboutMe";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/aboutMabe" element={<AboutSection />} />
+          <Route path="/aboutErick" element={<AboutErick />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
